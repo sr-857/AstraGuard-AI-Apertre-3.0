698eac4cea243cb7c0d421a01c47d5f6a6b078b2
Purvansh Joshi <purvanshjoshi7534011576@gmail.com>
2026-02-16 23:05:25 +0530

    feat(security): Implement Content Security Policy (CSP) Headers (#336)

--- ui/frontend/as_lp/next.config.mjs
+++ ui/frontend/as_lp/next.config.mjs
@@ -7,6 +7,35 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+              key: 'Permissions-Policy',
+              value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          }
+        ],
+      },
+    ]
+  },
 
 }
 
